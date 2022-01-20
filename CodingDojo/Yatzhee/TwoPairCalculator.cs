06bda73ace337caab6ea2ffce498783892e4672e
Niklas <nicke_lin_91@hotmail.com>
2022-01-20 13:50:33 +0100

    Two pair calculation

---
+++ CodingDojo/Yatzhee/TwoPairCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yatzhee
+{
+    public class TwoPairCalculator
+    {
+        public int Calculate(Roll roll)
+        {
+            int score = 0;
+
+            for (int i = 6; i > 0; i--)
+            {
+                var resultat = roll.Dices.Count(d => d.Number == i);
+                if (resultat >= 2)
+                {
+                    if(score > 0)
+                        return score + i * 2;
+                    score += i * 2;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
--- CodingDojo/YatzheeTest/TwoPairCalculatorTest.cs
+++ CodingDojo/YatzheeTest/TwoPairCalculatorTest.cs
@@ -22,5 +22,32 @@ namespace Yatzhee
             //Assert 
             Assert.That(actual, Is.EqualTo(0));
         }
+
+        [Test]
+        public void Calculate_2_2_4_4_6_Returns12()
+        {
+            //Arrange
+            var roll = new Roll(2,2, 4, 4, 6);
+            var sut = new TwoPairCalculator();
+
+            //Act
+            var actual = sut.Calculate(roll);
+
+            //Assert 
+            Assert.That(actual, Is.EqualTo(12));
+        }
+        [Test]
+        public void Calculate_2_2_3_4_6_Returns0()
+        {
+            //Arrange
+            var roll = new Roll(2, 2, 3, 4, 6);
+            var sut = new TwoPairCalculator();
+
+            //Act
+            var actual = sut.Calculate(roll);
+
+            //Assert 
+            Assert.That(actual, Is.EqualTo(0));
+        }
     }
 }
