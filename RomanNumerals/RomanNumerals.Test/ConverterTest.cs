72b6841c9cb15ea4718162943586e1568df39249
Stefan Cronert <cronert@yahoo.com>
2024-01-08 12:36:32 +0100

    Döpt om och skapat ett test som inte lyckas

---
+++ RomanNumerals/RomanNumerals.Test/ConverterTest.cs
@@ -0,0 +1,21 @@
+namespace RomanNumerals
+{
+    public class ConverterTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Convert_1_Returns_I()
+        {
+            //Arrange
+            var sut = new Converter();
+
+            //Act
+
+            //Assert
+        }
+    }
+}
