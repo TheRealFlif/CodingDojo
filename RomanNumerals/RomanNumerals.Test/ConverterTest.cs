bbe43a644a8b51cb548d2b14ed79f5f3bd4ddfa9
Stefan Cronert <cronert@yahoo.com>
2024-01-09 09:52:21 +0100

    Lagt till tester för att testa den senaste refaktoreringen

--- RomanNumerals/RomanNumerals.Test/ConverterTest.cs
+++ RomanNumerals/RomanNumerals.Test/ConverterTest.cs
@@ -90,31 +90,47 @@ public class ConverterTest
         {
             get
             {
-                yield return new TestCaseData(1).Returns("I"); 
-                yield return new TestCaseData(2).Returns("II");
-                yield return new TestCaseData(3).Returns("III");
-                yield return new TestCaseData(4).Returns("IV");
-                yield return new TestCaseData(5).Returns("V");
-                yield return new TestCaseData(6).Returns("VI");
-                yield return new TestCaseData(7).Returns("VII");
-                yield return new TestCaseData(8).Returns("VIII");
-                yield return new TestCaseData(9).Returns("IX");
-                yield return new TestCaseData(10).Returns("X");
-                yield return new TestCaseData(11).Returns("XI");
-                yield return new TestCaseData(12).Returns("XII");
-                yield return new TestCaseData(13).Returns("XIII");
-                yield return new TestCaseData(14).Returns("XIV");
-                yield return new TestCaseData(15).Returns("XV");
-                yield return new TestCaseData(16).Returns("XVI");
-                yield return new TestCaseData(17).Returns("XVII");
-                yield return new TestCaseData(18).Returns("XVIII");
+                yield return new TestCaseData(1).Returns("I").SetName("1 -> I"); 
+                yield return new TestCaseData(2).Returns("II").SetName("2 -> II");
+                yield return new TestCaseData(3).Returns("III").SetName("3 -> III");
+                yield return new TestCaseData(4).Returns("IV").SetName("4 -> IV");
+                yield return new TestCaseData(5).Returns("V").SetName("5 -> V");
+                yield return new TestCaseData(6).Returns("VI").SetName("6 -> VI");
+                yield return new TestCaseData(7).Returns("VII").SetName("7 -> VII");
+                yield return new TestCaseData(8).Returns("VIII").SetName("8 -> VIII");
+                yield return new TestCaseData(9).Returns("IX").SetName("9 -> IX");
+                yield return new TestCaseData(10).Returns("X").SetName("10 -> X");
+                yield return new TestCaseData(11).Returns("XI").SetName("11 -> XI");
+                yield return new TestCaseData(12).Returns("XII").SetName("12 -> XII");
+                yield return new TestCaseData(13).Returns("XIII").SetName("13 -> XIII");
+                yield return new TestCaseData(14).Returns("XIV").SetName("14 -> XIV");
+                yield return new TestCaseData(15).Returns("XV").SetName("15 -> XV");
+                yield return new TestCaseData(16).Returns("XVI").SetName("16 -> XVI");
+                yield return new TestCaseData(17).Returns("XVII").SetName("17 -> XVII");
+                yield return new TestCaseData(18).Returns("XVIII").SetName("18 -> XVIII");
+                yield return new TestCaseData(19).Returns("XIX").SetName("19 -> XIX");
+                yield return new TestCaseData(20).Returns("XX").SetName("20 -> XX");
+                yield return new TestCaseData(21).Returns("XXI").SetName("21 -> XXI");
+                yield return new TestCaseData(22).Returns("XXII").SetName("22 -> XXII");
+                yield return new TestCaseData(23).Returns("XXIII").SetName("23 -> XXIII");
+                yield return new TestCaseData(24).Returns("XXIV").SetName("24 -> XXIV");
+                yield return new TestCaseData(25).Returns("XXV").SetName("25 -> XXV");
+                yield return new TestCaseData(26).Returns("XXVI").SetName("26 -> XXVI");
+                yield return new TestCaseData(27).Returns("XXVII").SetName("27 -> XXVII");
+                yield return new TestCaseData(28).Returns("XXVIII").SetName("28 -> XXVIII");
+                yield return new TestCaseData(29).Returns("XXIX").SetName("29 -> XXIX");
+                yield return new TestCaseData(30).Returns("XXX").SetName("30 -> XXX");
 
-                yield return new TestCaseData(20).Returns("XX");
-                yield return new TestCaseData(30).Returns("XXX");
-                
-                yield return new TestCaseData(1000).Returns("M");
-                yield return new TestCaseData(2000).Returns("MM");
-                yield return new TestCaseData(3000).Returns("MMM");
+                yield return new TestCaseData(40).Returns("XL").SetName("40 -> XL");
+                yield return new TestCaseData(50).Returns("L").SetName("50 -> L");
+                yield return new TestCaseData(60).Returns("LX").SetName("60 -> LX");
+                yield return new TestCaseData(70).Returns("LXX").SetName("70 -> LXX");
+                yield return new TestCaseData(80).Returns("LXXX").SetName("80 -> LXXX");
+                yield return new TestCaseData(90).Returns("XC").SetName("90 -> XC");
+
+                yield return new TestCaseData(1000).Returns("M").SetName("1000 -> M");
+                yield return new TestCaseData(2000).Returns("MM").SetName("2000 -> MM");
+                yield return new TestCaseData(3000).Returns("MMM").SetName("3000 -> MMM");
                 yield return new TestCaseData(4000).Returns("MMMM");
             }
         }
