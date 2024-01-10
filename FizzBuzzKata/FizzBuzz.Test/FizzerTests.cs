faf1f657860a5565a877c737e833cf7e94cc1d17
Stefan Cronert <cronert@yahoo.com>
2024-01-10 16:40:56 +0100

    Lagt till att Fizzern returnerar Fizz även då talet innehåller tre

--- FizzBuzzKata/FizzBuzz.Test/FizzerTests.cs
+++ FizzBuzzKata/FizzBuzz.Test/FizzerTests.cs
@@ -25,6 +25,24 @@ public class FizzerTests
                 yield return new TestCaseData(10).Returns("10").SetName("10 => 10");
                 yield return new TestCaseData(11).Returns("11").SetName("11 => 11");
                 yield return new TestCaseData(12).Returns("Fizz").SetName("12 => Fizz");
+                yield return new TestCaseData(13).Returns("Fizz").SetName("13 => Fizz");
+                yield return new TestCaseData(14).Returns("14").SetName("14 => 14");
+                yield return new TestCaseData(15).Returns("Fizz").SetName("15 => Fizz");
+                yield return new TestCaseData(16).Returns("16").SetName("16 => 16");
+                yield return new TestCaseData(17).Returns("17").SetName("17 => 17");
+                yield return new TestCaseData(18).Returns("Fizz").SetName("18 => Fizz");
+                yield return new TestCaseData(19).Returns("19").SetName("19 => 19");
+                yield return new TestCaseData(20).Returns("20").SetName("20 => 20");
+                yield return new TestCaseData(21).Returns("Fizz").SetName("21 => Fizz");
+                yield return new TestCaseData(22).Returns("22").SetName("22 => 22");
+                yield return new TestCaseData(23).Returns("Fizz").SetName("23 => Fizz");
+                yield return new TestCaseData(24).Returns("Fizz").SetName("24 => Fizz");
+                yield return new TestCaseData(25).Returns("25").SetName("25 => 25");
+                yield return new TestCaseData(26).Returns("26").SetName("26 => 26");
+                yield return new TestCaseData(27).Returns("Fizz").SetName("27 => Fizz");
+                yield return new TestCaseData(28).Returns("28").SetName("28 => 28");
+                yield return new TestCaseData(29).Returns("29").SetName("29 => 29");
+                yield return new TestCaseData(30).Returns("Fizz").SetName("30 => Fizz");
             } 
         }
     }
--- FizzBuzzKata/FizzBuzz/Fizzer.cs
+++ FizzBuzzKata/FizzBuzz/Fizzer.cs
@@ -2,9 +2,15 @@
 {
     public class Fizzer
     {
+        private int _valueToScramble = 3;
+
         public string Scramble(int value)
         {
-            return value % 3 == 0
+            var scramble = 
+                value % 3 == 0 || 
+                value.ToString().Contains(_valueToScramble.ToString());
+
+            return scramble
                 ? "Fizz"
                 : value.ToString();
         }
