6ff963f47cdeadad563692dec06bdef505647a95
Stefan Cronert <cronert@yahoo.com>
2024-01-10 12:25:38 +0100

    Lagt till logik för att hantera summering av ental och tester av dessa

--- RomanCalculatorKata/RomanCalculator.Test/RomanCalculatorTests.cs
+++ RomanCalculatorKata/RomanCalculator.Test/RomanCalculatorTests.cs
@@ -31,6 +31,18 @@ public class RomanCalculatorTests
                 yield return new TestCaseData(string.Empty, string.Empty).Returns(string.Empty).SetName("Empty + Empty = Empty");
                 yield return new TestCaseData(string.Empty, "I").Returns("I").SetName("Empty + I = I");
                 yield return new TestCaseData("I", string.Empty).Returns("I").SetName("I + Empty = I");
+                yield return new TestCaseData("I", "I").Returns("II").SetName("I + I = II");
+                yield return new TestCaseData("II", "I").Returns("III").SetName("II + I = III");
+                yield return new TestCaseData("III", "I").Returns("IV").SetName("III + I = IV");
+                yield return new TestCaseData("IV", "I").Returns("V").SetName("IV + I = V");
+                yield return new TestCaseData("V", "I").Returns("VI").SetName("V + I = VI");
+                yield return new TestCaseData("VI", "I").Returns("VII").SetName("VI + I = VII");
+                yield return new TestCaseData("VII", "I").Returns("VIII").SetName("V + III = VIII");
+                yield return new TestCaseData("VIII", "I").Returns("IX").SetName("VIII + I = IX");
+                yield return new TestCaseData("II", "II").Returns("IV").SetName("II + II = IV");
+                yield return new TestCaseData("III", "III").Returns("VI").SetName("III + III = VI");
+                yield return new TestCaseData("IV", "IV").Returns("VIII").SetName("IV + IV = VIII");
+                yield return new TestCaseData("V", "IV").Returns("IX").SetName("V + IV = IX");
             }
         }
     }
--- RomanCalculatorKata/RomanCalculator/RomanCalculator.cs
+++ RomanCalculatorKata/RomanCalculator/RomanCalculator.cs
@@ -4,8 +4,56 @@ public class RomanCalculator
 {
     public string Add(string operand1, string operand2)
     {
-        return string.IsNullOrEmpty(operand1)
-            ? operand2 
-            : operand1;
+        var returnValue = AddUnit(operand1, operand2, out var carrie);
+        return returnValue;
     }
+
+    private static string AddUnit(
+        string operand1, 
+        string operand2, 
+        out string carrie) 
+    {
+        var unit1 = GetUnitPart(operand1);
+        var unit2 = GetUnitPart(operand2);
+        var sum = unit1 + unit2;
+        
+        carrie = (sum > 9)
+            ? "X"
+            : string.Empty;
+
+        return ConvertUnitToRomanNumeral(sum%10);
+    }
+
+    private static int GetUnitPart(string operand)
+    {
+        if (operand.EndsWith("IX")) return 9;
+        if (operand.EndsWith("VIII")) return 8;
+        if (operand.EndsWith("VII")) return 7;
+        if (operand.EndsWith("VI")) return 6;
+        if (operand.EndsWith("IV")) return 4;
+        if (operand.EndsWith("V")) return 5;
+        if (operand.EndsWith("III")) return 3;
+        if (operand.EndsWith("II")) return 2;
+        if (operand.EndsWith("I")) return 1;
+
+        return 0;
+    }
+
+    private static string ConvertUnitToRomanNumeral(int operand)
+    {
+        return operand switch
+        {
+            1 => "I",
+            2 => "II",
+            3 => "III",
+            4 => "IV",
+            5 => "V",
+            6 => "VI",
+            7 => "VII",
+            8 => "VIII",
+            9 => "IX",
+            _ => string.Empty,
+        };
+    }
+
 }
