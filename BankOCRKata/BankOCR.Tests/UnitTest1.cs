6b2d2494e298e6dc2e6f9966c040c3dacf100317
Stefan Cronert <cronert@yahoo.com>
2024-01-11 12:01:33 +0100

    Skapat solution, och projekt för logik och enhetstester

---
+++ BankOCRKata/BankOCR.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BankOCR.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
