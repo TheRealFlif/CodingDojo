05cca266e3df6042162b7b3528b11d32dd05353c
Stefan Cronert <cronert@yahoo.com>
2024-01-19 13:14:03 +0100

    Lagt till metdoen Add och enum Position för att koppla ihop två celler med varandra Lagt till ett test för att kontrollera att två olika celler kastar exception och att det inte är ett problem att lägga till samma cell två gånger till samma position

--- GameOfLifeKata/GameOfLife.Test/CellTests.cs
+++ GameOfLifeKata/GameOfLife.Test/CellTests.cs
@@ -14,4 +14,51 @@ public class CellTests
         //Assert
         Assert.That(actual, Is.EqualTo(0));
     }
+
+    [Test]
+    public void Add_NeighbourAtSouth_ThisIsAtNorth()
+    {
+        //Arrange
+        var sut = new Cell();
+        var neighbour = new Cell();
+
+        //Act
+        sut.Add(Position.South, neighbour);
+
+        //Assert
+        Assert.That(sut.At(Position.South), Is.EqualTo(neighbour));
+        Assert.That(neighbour.At(Position.North), Is.EqualTo(sut));
+    }
+
+    [Test]
+    public void Add_NeighbourAtSouthTwice_ThisIsAtNorth()
+    {
+        //Arrange
+        var sut = new Cell();
+        var neighbour = new Cell();
+
+        //Act
+        sut.Add(Position.South, neighbour);
+        sut.Add(Position.South, neighbour);
+
+        //Assert
+        Assert.That(sut.At(Position.South), Is.EqualTo(neighbour));
+        Assert.That(neighbour.At(Position.North), Is.EqualTo(sut));
+    }
+
+    [Test]
+    public void Add_TwoDifferentCellsToSamePosition_ThrowsException()
+    {
+        //Arrange
+        var sut = new Cell();
+        var neighbour = new Cell();
+        var anOtherNeighbour = new Cell();
+
+        //Act
+        sut.Add(Position.South, neighbour);
+        var actual = Assert.Throws<Exception>(() => sut.Add(Position.South, anOtherNeighbour));
+
+        //Assert
+        Assert.That(actual.Message, Is.EqualTo("Det finns redan ett item på position 'South'"));
+    }
 }
---
+++ GameOfLifeKata/GameOfLife/Position.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLife
+{
+    public enum Position
+    {
+        Unknown,
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
+}
