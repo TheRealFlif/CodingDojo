6e155f6f70489fbd2aeb5c34863b24003c256ef4
Stefan Cronert <cronert@yahoo.com>
2024-01-19 13:14:03 +0100

    Lagt till metdoen Add och enum Position för att koppla ihop två celler med varandra

--- GameOfLifeKata/GameOfLife.Test/CellTests.cs
+++ GameOfLifeKata/GameOfLife.Test/CellTests.cs
@@ -14,4 +14,19 @@ public class CellTests
         //Assert
         Assert.That(actual, Is.EqualTo(0));
     }
+
+    [Test]
+    public void Add_NeighbourAtSouth_ThisIsAtNorth()
+    {
+        //Arrange
+        var sut = new Cell();
+        var neighbour = new Cell();
+
+        //Act
+        sut.Add(Position.South, neighbour);
+
+        //Assert
+        Assert.That(sut.At(Position.South), Is.EqualTo(neighbour));
+        Assert.That(neighbour.At(Position.North), Is.EqualTo(sut));
+    }
 }
